e552dbba53d44a653795db86159c4d8eb7344c76
Ludger Goeminne <ludgergoeminne@gmail.com>
2024-08-08 16:32:32 -0400

    Update kMeansEqual.R

--- R/kMeansEqual.R
+++ R/kMeansEqual.R
@@ -8,74 +8,85 @@
 #' If `TRUE`, an attribute named `"centers"`  will be added to the output that contains the data frame with the new cluster centers. 
 #' This attribute can immediately be plugged into the `centers` argument if this function is to be executed iteratively. Defaults to `FALSE`.
 #' @param assigned.to A character vector indicating the name of the column that will contain the cluster identifier. Defaults to `"assigned"`.
+#' @examples
+#' data(mtcars)
+#' kdat <- mtcars %>% dplyr::select(c(mpg, wt))
+#' res <- kMeansEqual(kdat = kdat, k = 3, add.centers = TRUE)
+#' 
+#' # Get the data frame with the assigned clusters
+#' res
+#' # View the cluster centers
+#' attr(res, "centers")
+#' 
+#' @export
 kMeansEqual <- function(kdat, k, centers = NULL, add.centers = FALSE, assigned.to = "assigned"){
+
+if(ncol(kdat) != 2){
+  stop("This function requires a data frame with 2 columns as input!")
+}
+kdat %>% kmeans(k) -> kclust
+
+if(is.null(centers)){
+  centers <- kclust$centers
+} else{
+  if(ncol(centers) != 2){stop("Data frame `centers` needs to have exactly two rows.")}
+  if(nrow(centers) != k){stop("Data frame `centers` needs to have exactly `k` columns.")}
+}
+
+kdat[, paste0("D", 1:nrow(centers))] <- NA
+
+# seeds <- c(24778611, 99406234, 8194459, 51007850, 23271768, 59799526, 49374854, 38096642, 8162493, 26215659)
+for(i in 1:nrow(centers)){
+  # set.seed(seeds[i])
+  kdat[, paste0("D", i)] <- kdist(kdat[, 1], kdat[, 2], centers[i,1], centers[i,2])
+}
+
+kdat[, assigned.to] <- 0
+kdat$index <- 1:nrow(kdat)
+working <- kdat
+FirstRound <- nrow(kdat) - (nrow(kdat) %% k)
+
+j.vector <- sapply(1:FirstRound, function(z){
+  z <- if(z %% k == 0) k else (z %% k)
+  return(z)
+})
+
+for(j in j.vector){
+  # Cluster counts can be off by 1 due to uneven multiples of k. 
+  itemloc <- which.min(kdat[kdat[, assigned.to] == 0, (paste0("D", j))])[1]
+  kdat[which(kdat[, assigned.to] == 0)[itemloc], assigned.to] <- j
+  ### The sorting hat says... GRYFFINDOR!!! 
+}
+
+working <- kdat[kdat[, assigned.to] == 0,]
+for(i in 1:nrow(working)){
+  # These leftover points get assigned to whoever's closest, without regard to k
+  kdat[kdat$index == working$index[i], assigned.to] <- which(working[i, ((1:k)+2)] == min(working[i, ((1:k)+2)]))
+}
+
+if(add.centers){
+  NewCenters <- data.frame(x = rep(NA, k),
+                           y = rep(NA, k))
   
-  if(ncol(kdat) != 2){
-    stop("This function requires a data frame with 2 columns as input!")
-  }
-  kdat %>% kmeans(k) -> kclust
-  
-  if(is.null(centers)){
-    centers <- kclust$centers
-  } else{
-    if(ncol(centers) != 2){stop("Data frame `centers` needs to have exactly two rows.")}
-    if(nrow(centers) != k){stop("Data frame `centers` needs to have exactly `k` columns.")}
-  }
-  
-  kdat[, paste0("D", 1:nrow(centers))] <- NA
-  
-  # seeds <- c(24778611, 99406234, 8194459, 51007850, 23271768, 59799526, 49374854, 38096642, 8162493, 26215659)
-  for(i in 1:nrow(centers)){
-    # set.seed(seeds[i])
-    kdat[, paste0("D", i)] <- kdist(kdat[, 1], kdat[, 2], centers[i,1], centers[i,2])
-  }
-  
-  kdat[, assigned.to] <- 0
-  kdat$index <- 1:nrow(kdat)
-  working <- kdat
-  FirstRound <- nrow(kdat) - (nrow(kdat) %% k)
-  
-  j.vector <- sapply(1:FirstRound, function(z){
-    z <- if(z %% k == 0) k else (z %% k)
-    return(z)
-  })
-  
-  for(j in j.vector){
-    # Cluster counts can be off by 1 due to uneven multiples of k. 
-    itemloc <- which.min(kdat[kdat[, assigned.to] == 0, (paste0("D", j))])[1]
-    kdat[which(kdat[, assigned.to] == 0)[itemloc], assigned.to] <- j
-    ### The sorting hat says... GRYFFINDOR!!! 
-  }
-  
-  working <- kdat[kdat[, assigned.to] == 0,]
-  for(i in 1:nrow(working)){
-    # These leftover points get assigned to whoever's closest, without regard to k
-    kdat[kdat$index == working$index[i], assigned.to] <- which(working[i, ((1:k)+2)] == min(working[i, ((1:k)+2)]))
-  }
-  
-  if(add.centers){
-    NewCenters <- data.frame(x = rep(NA, k),
-                             y = rep(NA, k))
-    
-    for(i in 1:nrow(NewCenters)){
-      NewCenters[i, ] <- (kdat %>% filter(get(assigned.to) == i) %>% 
-                            `[`(c(1, 2)) %>%
-                            kmeans(1))$centers
-    }
-    
-    # Give original column names to NewCenters data frame
-    colnames(NewCenters) <- colnames(kdat)[c(1, 2)]
+  for(i in 1:nrow(NewCenters)){
+    NewCenters[i, ] <- (kdat %>% filter(get(assigned.to) == i) %>% 
+                          `[`(c(1, 2)) %>%
+                          kmeans(1))$centers
   }
   
-  # Only keep the original columns and the "assigned" column
-  kdat <- kdat[, c(1, 2, which(colnames(kdat) == assigned.to))]
-  
-  # Add an attribute "centers" in case we want to apply this function iteratively
-  if(add.centers){
-    attr(kdat, "centers") <- NewCenters
-  }
-  
-  return(kdat)
+  # Give original column names to NewCenters data frame
+  colnames(NewCenters) <- colnames(kdat)[c(1, 2)]
+}
+
+# Only keep the original columns and the "assigned" column
+kdat <- kdat[, c(1, 2, which(colnames(kdat) == assigned.to))]
+
+# Add an attribute "centers" in case we want to apply this function iteratively
+if(add.centers){
+  attr(kdat, "centers") <- NewCenters
+}
+
+return(kdat)
 }
 
 # Helper function
